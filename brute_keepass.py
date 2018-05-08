50fc7c2f374744347e2fad379061aae070a6dce8
Matt Smith <mlsmith@Matts-MacBook-Air.local>
2018-05-08 23:20:34 +1000

    Added word length loop

--- brute_keepass.py
+++ brute_keepass.py
@@ -6,6 +6,7 @@ import libkeepass
 
 words=[]
 INFILE= sys.argv[1]
+FOUND=0
 
 def unlock(INFILE, secret):
    with libkeepass.open(INFILE, password=secret, unprotect=False) as kdb:
@@ -19,11 +20,17 @@ for line in sys.stdin:
    words.append(line.rstrip())
 
 print("Trying brute force %s with the following words: %s" % (INFILE,words))
-for attempt in itertools.permutations(words,5):
-   print("Trying password %s" % ''.join(attempt))
-   try:
-      unlock(INFILE, ''.join(attempt))
-      break
-   except:
-      continue
+
+while FOUND==0:
+   for i in range(3,6):
+      for attempt in itertools.permutations(words,i):
+         print("Trying password %s" % ''.join(attempt))
+         try:
+            unlock(INFILE, ''.join(attempt))
+            FOUND=1
+            break
+         except KeyboardInterrupt:
+            raise
+         except: 
+            continue
 
